8ad0ed78f5ca9fb4e879be86a9914e44dfa332f9
Dan Clarke <dan.clarke@brainjuicer.com>
2016-04-20 16:20:45 +0100

    had to disable some bundled content that wasn't included

--- AppData/App_Start/BundleConfig.cs
+++ AppData/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace AppData
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
@@ -21,7 +21,7 @@ namespace AppData
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"));*/
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
