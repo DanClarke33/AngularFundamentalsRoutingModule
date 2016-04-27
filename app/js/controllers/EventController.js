a62a55f0d091d91824251015eaee8d5ef9289671
Dan Clarke <dan.clarke@brainjuicer.com>
2016-04-27 17:16:18 +0100

    using resolve during routing to get nicer loading of the route

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,13 +1,12 @@
 'use strict';
 
 eventsApp.controller('EventController', 
-    function EventController($scope, eventData, $anchorScroll, $routeParams) {
+    function EventController($scope, eventData, $anchorScroll, $routeParams, $route) {
         $scope.sortorder = 'name';
-        eventData.getEvent($routeParams.eventId)
-            .$promise
+        /*$route.current.locals.event()
             .then(function(event) { $scope.event = event; })
-            .catch(function(response) { console.log(response);}
-        );
+            .catch(function(response) { console.log(response);});*/
+        $scope.event = $route.current.locals.event;
 
         $scope.upVoteSession = function(session) {
           session.upVoteCount++;
