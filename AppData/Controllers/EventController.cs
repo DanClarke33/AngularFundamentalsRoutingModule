f414681bce92fed33ce52bc2ae492ccd6394dca2
Dan Clarke <dan.clarke@brainjuicer.com>
2016-04-27 14:10:16 +0100

    more routing stuff

--- AppData/Controllers/EventController.cs
+++ AppData/Controllers/EventController.cs
@@ -9,18 +9,34 @@ using Newtonsoft.Json.Linq;
 
 namespace AppData.Controllers
 {
+    using System.IO;
+
     public class EventController : ApiController
     {
         public JToken Get(string id = null)
         {
-            var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
-            return JObject.Parse(System.IO.File.ReadAllText(path + "../app/data/event/" + id + ".json"));
+            return id == null ? this.GetAllJsonEventsAsArray() : GetSingleJsonFile(id);
         }
 
         public void Post(string id, JObject eventData)
         {
             var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
-            System.IO.File.WriteAllText(path + "../app/data/event/" + id + ".json", eventData.ToString(Formatting.None));
+            File.WriteAllText(path + "../app/data/event/" + id + ".json", eventData.ToString(Formatting.None));
+        }
+
+        private static JToken GetSingleJsonFile(string id)
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
+            return JObject.Parse(System.IO.File.ReadAllText(path + "../app/data/event/" + id + ".json"));
+        }
+
+        private JArray GetAllJsonEventsAsArray()
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
+            var contents = Directory.GetFiles(path + "../app/data/event/")
+                .Aggregate(string.Empty, (current, file) => current + File.ReadAllText(file) + ",");
+
+            return JArray.Parse("[" + contents.Substring(0, contents.Length - 1) + "]");
         }
     }
 }
