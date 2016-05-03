6990cf2027220f0ef0ff4b410bac88a28ede754c
Dan Clarke <dan.clarke@brainjuicer.com>
2016-05-03 12:25:17 +0100

    first play with directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,2 +1,13 @@
 'use strict';
 
+eventsApp.directive('mySample', function($compile) {
+	return {
+		restrict: 'E', // allow directive to be used as an element ('C' for class, A' for attribute)
+		template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
+		/* example achieves the same as template but demonstrates the flexibility of the link property
+		link: function(scope, element, attrs, controller) {
+			var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>";
+			angular.element(element).html($compile(markup)(scope));
+		}*/
+	};
+});
