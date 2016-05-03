e320035edb5912e27e69b249cdf6aab8c1af8e86
Dan Clarke <dan.clarke@brainjuicer.com>
2016-05-03 12:38:42 +0100

    tidied up a little

--- app/js/directives/EventThumbnail.js
+++ app/js/directives/EventThumbnail.js
@@ -1,6 +1,7 @@
 eventsApp.directive('eventThumbnail', function() {
     return {
-        restrict: 'E',
+        restrict: 'E', // allow directive to be used as an element ('C' for class, A' for attribute)
+        replace: true, // replace the html element with the template html
         templateUrl: '/templates/directives/EventThumbnail.html'
     }
 });
