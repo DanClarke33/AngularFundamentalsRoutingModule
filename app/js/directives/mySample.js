d9ab18bdbcbaf9a70b485a56d17bcf6424670370
Dan Clarke <dan.clarke@brainjuicer.com>
2016-05-03 12:44:42 +0100

    demo of isolating the scope, try the SampleDirective.html

--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -1,7 +1,10 @@
 eventsApp.directive('mySample', function($compile) {
 	return {
-		restrict: 'E', // allow directive to be used as an element ('C' for class, A' for attribute)
-		template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
+		restrict: 'C', // allow directive to be used as an element ('C' for class, A' for attribute)
+		template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>",
+		scope: {
+			// isolate the scope by defining which scope to use and it's properties, in this case we don't need any
+		}
 		/* example achieves the same as template but demonstrates the flexibility of the link property
 		link: function(scope, element, attrs, controller) {
 			var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>";
