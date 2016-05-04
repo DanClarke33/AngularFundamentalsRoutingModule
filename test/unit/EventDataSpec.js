cd7e65e5bd66e0ec5df7ce4082f7dafe2e383e1e
Dan Clarke <dan.clarke@brainjuicer.com>
2016-05-04 15:45:51 +0100

    added tests for a service that makes ajax calls

---
+++ test/unit/EventDataSpec.js
@@ -0,0 +1,18 @@
+describe('eventData', function() {
+	beforeEach(module('eventsApp'));
+
+	// rather than mock $resource we really want to check that resource is used to make the desired call 
+	// but with a mock of the http behind it, $httpBackend provides this behaviour with the actual http calls mocked out
+	it('should issue a GET request to /data/event/[id] when getEvent is called with an id', 
+		inject(function(eventData, $httpBackend) {
+			$httpBackend.expectGET('/data/event/11');
+			$httpBackend.when('GET', '/data/event/11').respond({}); // setup behaviour
+
+			eventData.getEvent(11);
+			$httpBackend.flush(); //makes $resource service respond to the call
+
+			$httpBackend.verifyNoOutstandingExpectation();
+			$httpBackend.verifyNoOutstandingRequest(); 
+		})
+	);
+});
