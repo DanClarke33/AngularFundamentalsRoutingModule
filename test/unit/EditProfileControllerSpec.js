ab2b0a878c97a3d86ce74fa55ac22d321b362b5b
Dan Clarke <dan.clarke@brainjuicer.com>
2016-05-04 13:55:19 +0100

    made the test more complete to what we would test, it's not really enough to verify the build method was called, we do need to check the output, if anything you might justify checking the output without verifying the call to the dependency

--- test/unit/EditProfileControllerSpec.js
+++ test/unit/EditProfileControllerSpec.js
@@ -7,22 +7,26 @@ describe('EventProfileControllerSpec', function() {
 	beforeEach(inject(function($controller, $rootScope) {
 		$controllerConstructor = $controller; // take the controller constructor service and store it so we can use it in our test
 		scope = $rootScope.$new(); // create a scope object we can use in the test using the $rootscope service
-		mockGravatarUrlBuilder = sinon.stub({ buildGravatarUrl: function () { return "testurl"; }}); // create a stub using sinon which will give us mocking behaviour on this definition
+		mockGravatarUrlBuilder = sinon.stub({ buildGravatarUrl: function () {}}); // create a stub using sinon which will give us mocking behaviour on this definition
 	}));
 
 	it('should build the gravatar with the given email', function() {
+		var email = "joe@joe.com";
+		var url = "testurl";
+
+		// setup stub behaviour
+		mockGravatarUrlBuilder.buildGravatarUrl.withArgs(email).returns(url);
 
 		// create the actual controller to test, passing mock objects for its dependencies
 		$controllerConstructor("EditProfileController", 
 			{ '$scope': scope, gravatarUrlBuilder: mockGravatarUrlBuilder });
 
 		// check that on construction the desired effects occur
-		var email = "joe@joe.com";
 		var result = scope.getGravatarUrl(email);
 
 		// verify call
 		expect(mockGravatarUrlBuilder.buildGravatarUrl.calledWith(email)).toBe(true);
-		expect(result).toBe("testurl");
+		expect(result).toBe("url");
 	});
 
 });
